4a08e9c47d82fe8fa003a1ad9b49cbd52d23f180
Mesut <imesutkoca@gmail.com>
2021-01-17 17:50:45 +0300

    functions

---
+++ src/03-functions.ts
@@ -0,0 +1,35 @@
+/* eslint-disable */
+
+const add = (a: number, b: number) => {
+  return a + b;
+};
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const mutliply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const forecast = {
+  date: new Date(),
+  weather: "sunnny"
+};
+
+const logWeather = ({ date, weather }: { date: Date; weather: string }): void => {
+  console.log(date);
+  console.log(weather);
+};
